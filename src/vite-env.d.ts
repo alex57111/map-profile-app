3d1c6e8216f520e49d4b88c291301c72b5a72620
alex57111 <avan77333@gmail.com>
2026-07-04 22:22:04 +0300

    Add src/vite-env.d.ts

---
+++ src/vite-env.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_USE_SUPABASE: string
+  readonly VITE_SUPABASE_URL: string
+  readonly VITE_SUPABASE_ANON_KEY: string
+}
+interface ImportMeta { readonly env: ImportMetaEnv }
+declare module '*.png' { const src: string; export default src }
