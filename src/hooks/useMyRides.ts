bec5f480c5c4c82ab60954edafd686698714300c
alex57111 <avan77333@gmail.com>
2026-07-05 08:17:58 +0300

    Update src/hooks/useMyRides.ts

---
+++ src/hooks/useMyRides.ts
@@ -0,0 +1,4 @@
+
+export function useMyRides() {
+  return { asDriver: [], asPassenger: [], loading: false, cancelRide: async (_: string) => {}, respondRequest: async (_: string, __: string) => {}, reload: async () => {} }
+}
